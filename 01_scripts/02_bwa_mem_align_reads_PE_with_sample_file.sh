af34e0eac759bb3f4361d4f5ec4fb90bf93f05e9
Eric Normandeau <eric.normandeau.qc@gmail.com>
2019-07-26 09:08:57 -0400

    Debug 02_bwa_mem_align_reads_PE_with_sample_file.sh

--- 01_scripts/02_bwa_mem_align_reads_PE_with_sample_file.sh
+++ 01_scripts/02_bwa_mem_align_reads_PE_with_sample_file.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ## With GNU Parallel
-# ls -1 samples_split/* | parallel -k -j 10 srun -c 4 --mem 20G -p large --time 21-00:00 -J bwaMem -o 10-log_files/bwaMEMsplit_%j.log ./00-scripts/bwa_mem_align_reads_by_n_samples.sh 4 {} \; sleep 0.1 &
+# ls -1 samples_split/* | parallel -k -j 10 srun -c 4 --mem 20G -p large --time 21-00:00 -J bwaMem -o 99_log_files/bwaMEMsplit_%j.log ./01_scripts/02_bwa_mem_align_reads_PE_with_sample_file.sh 4 {} \; sleep 0.1 &
 
 ## srun
 # srun -c 4 --mem 20G -p large --time 21-00:00 -J bwaMem -o 10-log_files/bwaMEMsplit_%j.log ./00-scripts/bwa_mem_align_reads_by_n_samples.sh 4 <SAMPLE_FILE>
 
 # First split sample list to align into different files with:
-# cd 04-all_samples
-# ls -1 *.fq.gz > ../all_samples_for_alignment.txt
+# cd 05_trimmed
+# ls -1 *.fastq.gz > ../all_samples_for_alignment.txt
 # cd ..
 # mkdir samples_split
 # split -a 4 -l 100 -d all_samples_for_alignment.txt samples_split/samples_split.
@@ -18,8 +18,8 @@ GENOMEFOLDER="03_genome"
 GENOME="genome.fasta"
 RAWDATAFOLDER="05_trimmed"
 ALIGNEDFOLDER="06_aligned"
-SAMPLE_FILE="$1"
-NCPU=$2
+NCPU=$1
+SAMPLE_FILE="$2"
 
 # Test if user specified a number of CPUs
 if [[ -z "$NCPU" ]]
@@ -35,9 +35,6 @@ module load samtools/1.8
 #bwa index -p "$GENOMEFOLDER"/"$GENOME" "$GENOMEFOLDER"/"$GENOME"
 
 # Iterate over sequence file pairs and map with bwa
-for file in $(ls -1 "$RAWDATAFOLDER"/*_1.trimmed.fastq.gz)
-do
-
 cat "$SAMPLE_FILE" |
 while read file
 do
@@ -48,6 +45,10 @@ do
     name=$(basename "$file")
     name2=$(basename "$file2")
     ID="@RG\tID:ind\tSM:ind\tPL:Illumina"
+
+    echo "$name"
+    echo "$name2"
+    echo
 
     # Align reads
     bwa mem -t "$NCPU" -R "$ID" "$GENOMEFOLDER"/"$GENOME" "$RAWDATAFOLDER"/"$name" "$RAWDATAFOLDER"/"$name2" |
