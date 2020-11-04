37fefa4f266553b2c684434c315e82745d3ae93a
Eric Normandeau <eric.normandeau.qc@gmail.com>
2020-11-04 09:45:47 -0500

    Add reheader.sh script

---
+++ 01_scripts/reheader.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Re header all bam files to contain sample name
+
+# /!\ WARNING /!\
+# You may need to edit the perl command to keep the proper sample name
+
+for i in $(ls -1 *.bam | grep -v "\.reheader\.bam")
+do echo "$i"
+    sample=$(echo "$i" | perl -pe 's/_1\.trimmed.*//')
+    echo "$sample"
+    samtools view -H "$i" | sed "s/ind/$sample/g" | samtools reheader - "$i" > "${i%.bam}".reheader.bam
+done
